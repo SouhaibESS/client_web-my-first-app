3c8a320583dbb427d69a6146065b1fe902b29276
SouhaibESS <souhaibue@gmail.com>
2021-10-18 10:56:14 +0200

    using ref

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useRef, useState } from "react";
+import Component from "./components/Component";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const textInput = useRef(null);
+
+  const handleClick = () => {
+    setCounter(counter + 1);
+    alert(textInput.current.value);
+  };
+  
+
+  const buttonStyle = {
+    backgroundColor: "black",
+    color: "white",
+    fontSize: "20px",
+    padding: "10px 60px",
+    borderRadius: "5px",
+    margin: "10px 0px",
+    cursor: "pointer",
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/*}
+        <Component />
+        <button style={buttonStyle} onClick={handleClick}>
+          Click me!
+        </button> 
+        counter = {counter}
+        */}
+        <input type="text" ref={textInput} />
+        <input
+          type="button"
+          value="Focus the text input"
+          onClick={handleClick}
+        ></input>
       </header>
     </div>
   );
